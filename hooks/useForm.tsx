0d415c1c650e9c69849924e9e97889de877915f9
Donato Monzon <spartanhalokitty@gmail.com>
2022-08-21 20:13:41 -0500

    useForm upgraded: Now accept validations

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -1,9 +1,25 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 
-export function useForm<T> (initialValue: T) {
+type FormValidationType = {
+  [key: string]: {
+    validation: (value:string) => boolean,
+    message: string;
+  }
+}
+
+type Errors = {
+  [key: string]: string | null;
+}
+
+export function useForm<T> (initialValue: T, formValidations:FormValidationType = {}) {
   const [ formState, setFormState ] = useState<T>(initialValue)
+  const [ errors, setErrors ] = useState<Errors>({})
   
-  const onInputChange = ({target}:ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  useEffect(() => {
+    createValidators()
+  }, [formState])
+
+  const onInputChange = ({target}:ChangeEvent<HTMLInputElement | HTMLSelectElement  >) => {
     const {name, value} = target
     setFormState({
       ...formState,
@@ -15,10 +31,35 @@ export function useForm<T> (initialValue: T) {
     setFormState(initialValue)
   }
 
+  const isFormValid:boolean = useMemo(() => {
+    const errorKeys = Object.keys( errors )
+    
+    for( const key of errorKeys ) {
+      if( errors[key] !== null ) return false
+    }
+
+    return true
+  }, [errors])
+
+  const createValidators = () => {
+    const formCheckedValues: {[key:string]: string | null} = {}
+
+    for( const field of Object.keys(formValidations)) {
+      const { validation: validationFn, message } = formValidations[field]
+      const currentStateValue = (formState as unknown as { [key: string]: string })[field]
+      formCheckedValues[field] = validationFn( currentStateValue) ? null : message
+    }
+
+    setErrors( formCheckedValues )
+  }
+
   return {
     ...formState,
     formState,
     onInputChange,
-    onResetForm
+    onResetForm,
+    errors,
+    isFormValid,
   }
 }
+
