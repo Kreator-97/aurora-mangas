68606841099c757a71f0779ae58370e22c572a10
Donato Monzon <spartanhalokitty@gmail.com>
2022-08-23 13:05:38 -0500

    CardManga component now is using props instead static data

--- components/cardManga/CardManga.tsx
+++ components/cardManga/CardManga.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { FC } from 'react'
 import { HiOutlineHeart } from 'react-icons/hi'
+import { Manga } from '../../interfaces'
 
-export const CardManga = () => {
+interface Props {
+  manga: Manga;
+}
+
+export const CardManga:FC<Props> = ({manga}) => {
   const router = useRouter()
 
   const navigateTo = (url:string) => {
@@ -13,7 +19,7 @@ export const CardManga = () => {
   return (
     <div className="bg-white relative">
       <Image
-        src='/images/mangas/hxh-01.jpeg'
+        src={manga.imgURL}
         layout="responsive"
         width={ 170 }
         height={ 255 }
@@ -21,17 +27,17 @@ export const CardManga = () => {
         onClick={ () => navigateTo('/serie/hunter-x-hunter/01') }
       />
       <div className='p-2 flex flex-col items-center'>
-        <Link passHref href='/serie/hunter-x-hunter'>
-          <a className='text-dark text-xl text-center'>Hunter x Hunter</a>
+        <Link passHref href={`/serie/${manga.serie.name}`}>
+          <a className='text-dark text-xl text-center'>{ manga.serie.name } #{manga.number}</a>
         </Link>
-        <p className='text-dark text-lg'>$ 99.00</p>
+        <p className='text-dark text-lg'>$ { manga.price }</p>
         <button className='btn bg-accent text-dark w-full'>Agregar al carrito</button>
       </div>
       <div className='p-1 bg-light absolute top-4 right-2 rounded-full'>
         <HiOutlineHeart color='var(--dark)' className='' size={36}/>
       </div>
       <div className='absolute bg-dark bg-opacity-75 top-2/4 right-2 rounded-full'>
-        <p className='text-2xl p-2'># 1</p>
+        <p className='text-2xl p-2'># {manga.number}</p>
       </div>
     </div>
   )
